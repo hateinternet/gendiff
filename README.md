d500560dd4ac597ce49394d0598f4e2c2071ccbd
feofilov.ilya <feofilov.ilya@gmail.com>
2019-05-29 06:57:47 +0300

    add gendiff bin

---
+++ README.md
@@ -0,0 +1,9 @@
+# gendiff
+
+study in [hexlet.io](https://ru.hexlet.io/)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/9b75087cae81246a60f2/maintainability)](https://codeclimate.com/github/hateinternet/project-lvl2-s487/maintainability)
+
+## Setup & Usage
+
+[![asciicast](https://asciinema.org/a/c80rmi2kBXT2GeFywsxGwOWbr.svg)](https://asciinema.org/a/c80rmi2kBXT2GeFywsxGwOWbr)
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
-const some = 'you';
-console.log(`fuck ${some}`);
+import commander from 'commander';
+
+commander
+  .command('gendiff <firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'Output format')
+  .parse(process.argv);
