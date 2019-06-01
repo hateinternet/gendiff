b45634609f1b65a94077453b9f128636dd7c41ad
feofilov.ilya <feofilov.ilya@gmail.com>
2019-06-02 00:57:55 +0300

    add ini support

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,26 +1,32 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-test('genDiff 1 - json', () => {
-  const filepath1 = './__tests__/__fixtures__/before.json';
-  const filepath2 = './__tests__/__fixtures__/after.json';
-  const actual = genDiff(filepath1, filepath2);
-  const expected = fs.readFileSync('./__tests__/__fixtures__/jsonResult.txt', 'utf-8');
-  expect(actual).toBe(expected);
-});
+const table = [
+  [
+    './__tests__/__fixtures__/before.json',
+    './__tests__/__fixtures__/after.json',
+    fs.readFileSync('./__tests__/__fixtures__/jsonResult.txt', 'utf-8'),
+  ],
+  [
+    './__tests__/__fixtures__/before.yaml',
+    './__tests__/__fixtures__/after.yaml',
+    fs.readFileSync('./__tests__/__fixtures__/yamlResult.txt', 'utf-8'),
+  ],
+  [
+    './__tests__/__fixtures__/before.yml',
+    './__tests__/__fixtures__/after.yml',
+    fs.readFileSync('./__tests__/__fixtures__/yamlResult.txt', 'utf-8'),
+  ],
+  [
+    './__tests__/__fixtures__/before.ini',
+    './__tests__/__fixtures__/after.ini',
+    fs.readFileSync('./__tests__/__fixtures__/iniResult.txt', 'utf-8'),
+  ],
+];
 
-test('genDiff 2 - yml', () => {
-  const filepath1 = './__tests__/__fixtures__/before.yml';
-  const filepath2 = './__tests__/__fixtures__/after.yml';
-  const actual = genDiff(filepath1, filepath2);
-  const expected = fs.readFileSync('./__tests__/__fixtures__/yamlResult.txt', 'utf-8');
-  expect(actual).toBe(expected);
-});
-
-test('genDiff 3 - yaml', () => {
-  const filepath1 = './__tests__/__fixtures__/before.yaml';
-  const filepath2 = './__tests__/__fixtures__/after.yaml';
-  const actual = genDiff(filepath1, filepath2);
-  const expected = fs.readFileSync('./__tests__/__fixtures__/yamlResult.txt', 'utf-8');
-  expect(actual).toBe(expected);
-});
+test.each(table)(
+  'diff: %s %s',
+  (filepath1, filepath2, expected) => {
+    expect(genDiff(filepath1, filepath2)).toBe(expected);
+  },
+);
--- src/parsers.js
+++ src/parsers.js
@@ -1,9 +1,11 @@
 import { safeLoad } from 'js-yaml';
+import { parse } from 'ini';
 
 const parsers = {
   '.json': JSON.parse,
   '.yaml': safeLoad,
   '.yml': safeLoad,
+  '.ini': parse,
 };
 
 export default type => filedata => parsers[type](filedata);
