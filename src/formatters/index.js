f9edbf63451638397c0500edd7f5b7db2a1a411e
feofilov.ilya <feofilov.ilya@gmail.com>
2019-06-08 15:25:57 +0300

    add support json output

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,11 @@
 import toPlain from './plain';
 import toTree from './tree';
+import toJson from './json';
 
 const formatTypes = {
   plain: toPlain,
   tree: toTree,
+  json: toJson,
 };
 
 export default format => ast => formatTypes[format](ast);
