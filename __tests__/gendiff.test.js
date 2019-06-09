29e71d6467f931b95696895529c636cd077781d5
feofilov.ilya <feofilov.ilya@gmail.com>
2019-06-09 18:04:22 +0300

    tests fix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,47 +1,140 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const table = [
+const tableJson = [
   [
-    './__tests__/__fixtures__/before.json',
-    './__tests__/__fixtures__/after.json',
-    fs.readFileSync('./__tests__/__fixtures__/result.txt', 'utf-8'),
+    './__tests__/__fixtures__/json/before.json',
+    './__tests__/__fixtures__/json/after.json',
+    'tree',
+    fs.readFileSync('./__tests__/__fixtures__/json/resultTree.txt', 'utf-8'),
   ],
   [
-    './__tests__/__fixtures__/before.yaml',
-    './__tests__/__fixtures__/after.yaml',
-    fs.readFileSync('./__tests__/__fixtures__/result.txt', 'utf-8'),
+    './__tests__/__fixtures__/json/before.json',
+    './__tests__/__fixtures__/json/after.json',
+    'plain',
+    fs.readFileSync('./__tests__/__fixtures__/json/resultPlain.txt', 'utf-8'),
   ],
   [
-    './__tests__/__fixtures__/before.yml',
-    './__tests__/__fixtures__/after.yml',
-    fs.readFileSync('./__tests__/__fixtures__/result.txt', 'utf-8'),
+    './__tests__/__fixtures__/json/before.json',
+    './__tests__/__fixtures__/json/after.json',
+    'json',
+    fs.readFileSync('./__tests__/__fixtures__/json/resultJson.txt', 'utf-8'),
   ],
   [
-    './__tests__/__fixtures__/before.ini',
-    './__tests__/__fixtures__/after.ini',
-    fs.readFileSync('./__tests__/__fixtures__/result.txt', 'utf-8'),
+    './__tests__/__fixtures__/json/beforeDeep.json',
+    './__tests__/__fixtures__/json/afterDeep.json',
+    'tree',
+    fs.readFileSync('./__tests__/__fixtures__/json/resultTreeDeep.txt', 'utf-8'),
   ],
   [
-    './__tests__/__fixtures__/beforeDeep.json',
-    './__tests__/__fixtures__/afterDeep.json',
-    fs.readFileSync('./__tests__/__fixtures__/resultDeep.txt', 'utf-8'),
+    './__tests__/__fixtures__/json/beforeDeep.json',
+    './__tests__/__fixtures__/json/afterDeep.json',
+    'plain',
+    fs.readFileSync('./__tests__/__fixtures__/json/resultPlainDeep.txt', 'utf-8'),
   ],
   [
-    './__tests__/__fixtures__/beforeDeep.yaml',
-    './__tests__/__fixtures__/afterDeep.yaml',
-    fs.readFileSync('./__tests__/__fixtures__/resultDeep.txt', 'utf-8'),
-  ],
-  [
-    './__tests__/__fixtures__/beforeDeep.ini',
-    './__tests__/__fixtures__/afterDeep.ini',
-    fs.readFileSync('./__tests__/__fixtures__/resultDeep.txt', 'utf-8'),
+    './__tests__/__fixtures__/json/beforeDeep.json',
+    './__tests__/__fixtures__/json/afterDeep.json',
+    'json',
+    fs.readFileSync('./__tests__/__fixtures__/json/resultJsonDeep.txt', 'utf-8'),
   ],
 ];
 
-test.each(table)(
-  'diff: %s %s',
-  (filepath1, filepath2, expected) => {
-    expect(genDiff(filepath1, filepath2)).toBe(expected);
+test.each(tableJson)(
+  'diff from json: %s %s %s',
+  (filepath1, filepath2, format, expected) => {
+    expect(genDiff(filepath1, filepath2, format)).toBe(expected);
+  },
+);
+
+const tableYml = [
+  [
+    './__tests__/__fixtures__/yml/before.yml',
+    './__tests__/__fixtures__/yml/after.yml',
+    'tree',
+    fs.readFileSync('./__tests__/__fixtures__/yml/resultTree.txt', 'utf-8'),
+  ],
+  [
+    './__tests__/__fixtures__/yml/before.yml',
+    './__tests__/__fixtures__/yml/after.yml',
+    'plain',
+    fs.readFileSync('./__tests__/__fixtures__/yml/resultPlain.txt', 'utf-8'),
+  ],
+  [
+    './__tests__/__fixtures__/yml/before.yml',
+    './__tests__/__fixtures__/yml/after.yml',
+    'json',
+    fs.readFileSync('./__tests__/__fixtures__/yml/resultJson.txt', 'utf-8'),
+  ],
+  [
+    './__tests__/__fixtures__/yml/beforeDeep.yml',
+    './__tests__/__fixtures__/yml/afterDeep.yml',
+    'tree',
+    fs.readFileSync('./__tests__/__fixtures__/yml/resultTreeDeep.txt', 'utf-8'),
+  ],
+  [
+    './__tests__/__fixtures__/yml/beforeDeep.yml',
+    './__tests__/__fixtures__/yml/afterDeep.yml',
+    'plain',
+    fs.readFileSync('./__tests__/__fixtures__/yml/resultPlainDeep.txt', 'utf-8'),
+  ],
+  [
+    './__tests__/__fixtures__/yml/beforeDeep.yml',
+    './__tests__/__fixtures__/yml/afterDeep.yml',
+    'json',
+    fs.readFileSync('./__tests__/__fixtures__/yml/resultJsonDeep.txt', 'utf-8'),
+  ],
+];
+
+test.each(tableYml)(
+  'diff from yml: %s %s %s',
+  (filepath1, filepath2, format, expected) => {
+    expect(genDiff(filepath1, filepath2, format)).toBe(expected);
+  },
+);
+
+const tableIni = [
+  [
+    './__tests__/__fixtures__/ini/before.ini',
+    './__tests__/__fixtures__/ini/after.ini',
+    'tree',
+    fs.readFileSync('./__tests__/__fixtures__/ini/resultTree.txt', 'utf-8'),
+  ],
+  [
+    './__tests__/__fixtures__/ini/before.ini',
+    './__tests__/__fixtures__/ini/after.ini',
+    'plain',
+    fs.readFileSync('./__tests__/__fixtures__/ini/resultPlain.txt', 'utf-8'),
+  ],
+  [
+    './__tests__/__fixtures__/ini/before.ini',
+    './__tests__/__fixtures__/ini/after.ini',
+    'json',
+    fs.readFileSync('./__tests__/__fixtures__/ini/resultJson.txt', 'utf-8'),
+  ],
+  [
+    './__tests__/__fixtures__/ini/beforeDeep.ini',
+    './__tests__/__fixtures__/ini/afterDeep.ini',
+    'tree',
+    fs.readFileSync('./__tests__/__fixtures__/ini/resultTreeDeep.txt', 'utf-8'),
+  ],
+  [
+    './__tests__/__fixtures__/ini/beforeDeep.ini',
+    './__tests__/__fixtures__/ini/afterDeep.ini',
+    'plain',
+    fs.readFileSync('./__tests__/__fixtures__/ini/resultPlainDeep.txt', 'utf-8'),
+  ],
+  [
+    './__tests__/__fixtures__/ini/beforeDeep.ini',
+    './__tests__/__fixtures__/ini/afterDeep.ini',
+    'json',
+    fs.readFileSync('./__tests__/__fixtures__/ini/resultJsonDeep.txt', 'utf-8'),
+  ],
+];
+
+test.each(tableIni)(
+  'diff from json: %s %s %s',
+  (filepath1, filepath2, format, expected) => {
+    expect(genDiff(filepath1, filepath2, format)).toBe(expected);
   },
 );
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,22 +8,22 @@ const renderMethods = {
   added: (node, path) => {
     const { name, newValue } = node;
     const fullPath = getFullPath(path, name);
-    return `\nProperty '${fullPath}' was added with value: ${toString(newValue)}`;
+    return `Property '${fullPath}' was added with value: ${toString(newValue)}`;
   },
   removed: (node, path) => {
     const { name } = node;
     const fullPath = getFullPath(path, name);
-    return `\nProperty '${fullPath}' was removed`;
+    return `Property '${fullPath}' was removed`;
   },
   changed: (node, path) => {
     const { name, oldValue, newValue } = node;
     const fullPath = getFullPath(path, name);
-    return `\nProperty '${fullPath}' was updated. From ${toString(oldValue)} to ${toString(newValue)}`;
+    return `Property '${fullPath}' was updated. From ${toString(oldValue)} to ${toString(newValue)}`;
   },
   unchanged: (node, path) => {
     const { name, value } = node;
     const fullPath = getFullPath(path, name);
-    return `\nProperty '${fullPath}' hasn't been changed. It's equal to: ${toString(value)}`;
+    return `Property '${fullPath}' hasn't been changed. It's equal to: ${toString(value)}`;
   },
   joined: (node, path, fn) => {
     const { name, children } = node;
@@ -36,9 +36,9 @@ const rendering = (ast, path = []) => {
   const plainText = ast.reduce((str, node) => {
     const { operation } = node;
     const renderMethod = renderMethods[operation];
-    return `${str}${renderMethod(node, path, rendering)}`;
+    return `${str}\n${renderMethod(node, path, rendering)}`;
   }, '');
-  return plainText;
+  return plainText.trim();
 };
 
 export default rendering;
